966f07786c6d1317ff3b8c38fe7f80c0355b7d97
Natalie Cheng <nataliecheng2026@u.northwestern.edu>
2024-01-28 20:35:01 -0600

    add tmp and atlas

---
+++ Assets/Code/Atlas.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atlas : MonoBehaviour
+{
+    // vars
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    public static float health = 100;
+    public float speed = 2;
+
+    // call start
+    private void Start()
+    {
+        // initialize santa vars
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // frame update
+    private void Update()
+    {
+        Move();
+
+    }
+
+    // move and update sprite
+    private void Move()
+    {
+        // horizontal and vertical input axes
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // set the direction, increase by speed
+        Vector2 vec = new Vector2(horizontal, vertical); ;
+        rb.velocity = vec * speed;
+    }
+}
