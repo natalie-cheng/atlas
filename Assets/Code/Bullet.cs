42d6ee89023839e7591b68795e8756cce8d2698d
AlexShen21 <121585614+AlexShen21@users.noreply.github.com>
2024-02-04 19:34:45 -0600

    Update Bullet -Alex

--- Assets/Code/Bullet.cs
+++ Assets/Code/Bullet.cs
@@ -10,11 +10,15 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Vector2 position = FindAnyObjectByType<Atlas_Level3>().transform.position;
+
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = direction - position;
+        direction = direction.normalized;
         float x = direction.x;
         float y = direction.y;
         rb = GetComponent<Rigidbody2D>();
-        velocity = new Vector2(x * 0.4f, y * 0.4f);
+        velocity = new Vector2(x * 2f, y * 2f);
     }
 
     // Update is called once per frame
