e912367f6bb39ca54ea1df7aae8f46c026f61997
AlexShen21 <121585614+AlexShen21@users.noreply.github.com>
2024-02-10 14:36:01 -0600

    finished level 3 mechanics

--- Assets/Code/Level 3/Atlas_Level3.cs
+++ Assets/Code/Level 3/Atlas_Level3.cs
@@ -1,34 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Atlas_Level3 : MonoBehaviour
 {
     // vars
     private Rigidbody2D rb;
-    private SpriteRenderer spriteRenderer;
+    public SpriteRenderer spriteRenderer;
     public float health = 100;
-    public float speed = 2;
+    public float speed = 4;
     public GameObject bullet;
     public GameObject arrow;
-
+    private float lastShotTime;
+    public float reloadTime = 3f;
     // call start
     private void Start()
     {
         // initialize santa vars
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        lastShotTime = -reloadTime; 
     }
 
     // frame update
     private void Update()
     {
+        if (health <= 0)
+        {
+            die();
+        }
+
         Move();
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && Time.time - lastShotTime >= reloadTime)
         {
             Shoot();
+            lastShotTime = Time.time; // Update lastShotTime to the current time
         }
-        
+
+
+
+
 
     }
 
@@ -47,8 +59,27 @@ public class Atlas_Level3 : MonoBehaviour
     private void Shoot()
     {
         Vector2 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 position = gameObject.transform.position;
+        diff = diff - position;
         Vector2 direction = diff.normalized;
         Instantiate(bullet, rb.position + direction, Quaternion.identity);
     }
+
+    private void die()
+    {
+        Destroy(gameObject);
+        DestroyAllWithTag("Bird");
+        DestroyAllWithTag("Bird_1");
+    }
+
+    void DestroyAllWithTag(string tag)
+    {
+        GameObject[] objectsToDestroy = GameObject.FindGameObjectsWithTag(tag);
+        foreach (GameObject obj in objectsToDestroy)
+        {
+            Destroy(obj);
+        }
+    }
+
 
 }
